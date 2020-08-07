7109f698c0c054fd21e505bf75a7117509b7700e
morshedulmunna <morshedulmunna1@gmail.comm>
2020-08-08 03:04:30 +0600

    double-triple-equalr

---
+++ double-triple-equal.js
@@ -0,0 +1,20 @@
+// == means only value checking 
+// === means value and type both checking.  ( it's recommended)
+
+const first = 3
+const second = "3"  
+if (first == second) {
+    console.log("condition is true");    
+}
+else{
+    console.log("condition is false");
+}
+// === means value and type both checking.
+const first1 = 3
+const second2 = "3"  
+if (first1 === second2) {
+    console.log("condition is true");    
+}
+else{
+    console.log("condition is false");
+}
---
+++ true.js
@@ -0,0 +1,17 @@
+//false hobe below option gulo
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+// truthy 
+// '0', ' ' []
+
+const name = "Rahim" ;
+if (name.length){
+    console.log( "Condition is true")  
+}
+else{
+    console.log("condition is false")
+}
