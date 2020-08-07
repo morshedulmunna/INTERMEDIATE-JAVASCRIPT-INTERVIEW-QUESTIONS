953858b1bc3ba0a56477e58f017f3981b4ec2956
morshedulmunna <morshedulmunna1@gmail.comm>
2020-08-08 02:42:51 +0600

    null vs undefined inter

---
+++ null-vs-undefiend.js
@@ -0,0 +1,12 @@
+let name; // Result Undefined 
+console.log(name);
+
+function add(name1, name2) {
+    console.log(num1 + num2);
+}
+const result = add(23,34);  // if we are put only one value it's also undefined.
+console.log ( result);  // result  undefined  because we are not return anything in we are just type return then also it's undefined.
+
+let fun = undefined; // when variable value undefined result also undefined
+console.log(fun);
+
